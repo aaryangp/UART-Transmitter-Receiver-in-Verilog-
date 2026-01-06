46f83d8e09759d00ce20f4ef02c24dcda3226aeb
Aaryan Gupta <aaryangp31@gmail.com>
2026-01-06 23:26:31 +0530

    Add files via upload

---
+++ baudrategen.v
@@ -0,0 +1,32 @@
+module baudrategenerator(
+  input  clk,
+  input  rst,
+  output tx_enb,
+  output rx_enb
+);
+
+reg [12:0] tx_counter;
+reg [9:0]  rx_counter;
+
+always @(posedge clk) begin
+  if (rst)
+    tx_counter <= 0;
+  else if (tx_counter == 5207)
+    tx_counter <= 0;
+  else
+    tx_counter <= tx_counter + 1'b1;
+end
+
+always @(posedge clk) begin
+  if (rst)
+    rx_counter <= 0;
+  else if (rx_counter == 324)
+    rx_counter <= 0;
+  else
+    rx_counter <= rx_counter + 1'b1;
+end
+
+assign tx_enb = (tx_counter == 0);
+assign rx_enb = (rx_counter == 0);
+
+endmodule
